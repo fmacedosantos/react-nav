02733523551ae8458019bc033f7c494dfe76ddc4
fmacedosantos <felipemacedo986@gmail.com>
2024-08-16 10:13:03 -0300

    Utilizando nossas rotas em App

--- App.tsx
+++ App.tsx
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen';
+import { Routes } from './src/routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <HomeScreen/>
-      <StatusBar style="auto" />
-    </View>
+    // há que home é a primeira rota, ela aparece
+    <Routes/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
