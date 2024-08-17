bd083e3b9cdca130311380c00f868e745050e5f8
fmacedosantos <felipemacedo986@gmail.com>
2024-08-17 14:39:48 -0300

    Trabalhando com MaterialCommunityIcons

--- src/routes/bottom-tabs.routes.tsx
+++ src/routes/bottom-tabs.routes.tsx
@@ -1,14 +1,27 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeScreen from "../screens/HomeScreen";
 import GalleryScreen from "../screens/GalleryScreen";
+import {MaterialCommunityIcons} from '@expo/vector-icons'
 
 const {Navigator, Screen} = createBottomTabNavigator()
 
 export function BottomTabRoutes(){
     return(
         <Navigator>
-            <Screen name='home' component={HomeScreen}/>
-            <Screen name='gallery' component={GalleryScreen}/>
+            <Screen name='home' component={HomeScreen}
+            options={{
+                tabBarIcon: ({color, size}) => (
+                    <MaterialCommunityIcons name='home' color={color}
+                    size={size}/>
+                ),
+            }}/>
+            <Screen name='gallery' component={GalleryScreen}
+            options={{
+                tabBarIcon: ({color, size}) => (
+                    <MaterialCommunityIcons name='view-gallery' color='blue'
+                    size={43}/>
+                ),
+            }}/>
         </Navigator>
     )
 }
