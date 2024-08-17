f47577b7fd03084c755415089800b715596cbdf2
fmacedosantos <felipemacedo986@gmail.com>
2024-08-17 17:09:33 -0300

    Aplicando DrawerRoutes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,13 @@ import { NavigationContainer } from "@react-navigation/native";
 // contexto
 import { StackRoutes } from "./stack.routes";
 import { BottomTabRoutes } from "./bottom-tabs.routes";
+import { DrawerRoutes } from "./drawer.routes";
 
 // caixa de contexto
 export function Routes(){
     return(
         <NavigationContainer>
-            <BottomTabRoutes/>
+            <DrawerRoutes/>
         </NavigationContainer>
     )
 }
