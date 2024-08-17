9023d277d9e1df1a66ff506c0007b6e6a6383e9c
fmacedosantos <felipemacedo986@gmail.com>
2024-08-16 23:39:59 -0300

    Utilizando o bottom tabs

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 // contexto
 import { StackRoutes } from "./stack.routes";
+import { BottomTabRoutes } from "./bottom-tabs.routes";
 
 // caixa de contexto
 export function Routes(){
     return(
         <NavigationContainer>
-            <StackRoutes/>
+            <BottomTabRoutes/>
         </NavigationContainer>
     )
 }
